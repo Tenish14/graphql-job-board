f854784aff3d97b8a44c6821ada7c4cafe73b1f5
Tenish <Tenish14@gmail.com>
2022-08-26 13:33:50 +0800

    Setup graphql in Server file

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,5 +1,13 @@
+import { Company, Job } from './db.js'
+
 export const resolvers = {
     Query: {
-        greeting: () => 'Hello World!'
+        jobs: () => Job.findAll(),
+    },
+
+    Job: {
+        company: (job) => {
+            return Company.findById(job.companyId)
+        }
     }
 }
